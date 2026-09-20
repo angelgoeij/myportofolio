e6ca5f9f594526d75dea4e7f641712c98dae58db
angelgoeij <angelatika2000@gmail.com>
2026-09-21 01:26:31 +0700

    feat: add education URL routes

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import delete_project, get_projects_json, show_main, show_experience, show_projects, show_contact, create_project
+from main.views import create_education, delete_education, delete_project, get_education_json, get_projects_json, show_education, show_main, show_experience, show_projects, show_contact, create_project
 
 app_name = "main"
 
@@ -12,4 +12,10 @@ urlpatterns = [
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
     path("projects/<uuid:project_id>/delete/", delete_project, name="delete_project"),
+
+    path("education/", show_education, name="show_education"),
+    path("education/add", create_education, name = "create_education"),
+    path("api/education", get_education_json, name="get_projects_json"),
+    path("education/<uuid:education_id>/delete/", delete_education, name="delete_education"),
+
 ]
